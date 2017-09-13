a477ca41e381cfe0c21f6346106b51cf4ad69e78
yi1306c12@w2-712 <yi1306c12@gmail.com>
2017-09-13 03:21:57 -0400

    pendulum ok

--- agents/Unified_Neural_Model.cpp
+++ agents/Unified_Neural_Model.cpp
@@ -2,49 +2,25 @@
 
 #include<vector>
 using std::vector;
-/*
+
 const int self_connection = 100;
 const int other_connection = 5;
 vector<vector<int> > structure = {
-	{self_connection,other_connection,other_connection,0,0,0},
-	{0,self_connection,other_connection,other_connection,other_connection,0},
-	{0,other_connection,self_connection,other_connection,0,0},
-	{0,other_connection,0,self_connection,other_connection,0},
-	{0,other_connection,0,0,self_connection,other_connection},
-	{0,0,0,0,0,self_connection}
-};
-#include"modules/group_setting.h"
-//speed,type,modulation,hierarchie(underconstruction	),neuron_capacity
-const int max_n_group = 10;
-vector<group_setting> group_settings = {
-	{LEVEL1,{max_n_group,0,0,0,0},true,false,max_n_group},
-	{LEVEL1,{0,0,0,0,max_n_group},true,false,max_n_group},
-	{LEVEL49,{max_n_group,max_n_group,max_n_group,max_n_group,0},true,false,max_n_group},
-	{LEVEL7,{max_n_group,max_n_group,max_n_group,max_n_group,0},true,false,max_n_group},
-	{LEVEL1,{max_n_group,max_n_group,max_n_group,max_n_group,0},true,false,max_n_group},
-	{LEVEL1,{max_n_group,0,0,0,0},true,false,max_n_group}
-};
-*/
-const int self_connection = 100;
-const int other_connection = 5;
-vector<vector<int> > structure = {
-	{self_connection,other_connection,other_connection,0,0,0},
-	{0,self_connection,other_connection,other_connection,other_connection,0},
-	{0,other_connection,self_connection,other_connection,0,0},
-	{0,other_connection,0,self_connection,other_connection,0},
-	{0,other_connection,0,0,self_connection,other_connection},
-	{0,0,0,0,0,self_connection}
+	{self_connection,other_connection,other_connection,0},
+	{other_connection,self_connection,other_connection,other_connection},
+	{0,other_connection,self_connection,other_connection},
+	{0,other_connection,0,self_connection}
 };
 #include"modules/group_setting.h"
 //speed,type,modulation,hierarchie(underconstruction),neuron_capacity
 const int max_n_group = 10;
 vector<group_setting> group_settings = {
-	{LEVEL1,{max_n_group,0,0,0,0},true,false,max_n_group},
-	{LEVEL1,{0,0,0,0,max_n_group},true,false,max_n_group},
+	//{LEVEL1,{0,0,0,0,0},true,false,max_n_group},
 	{LEVEL49,{max_n_group,max_n_group,max_n_group,max_n_group,0},true,false,max_n_group},
+	{LEVEL1,{0,0,0,0,max_n_group},true,false,max_n_group},
 	{LEVEL7,{max_n_group,max_n_group,max_n_group,max_n_group,0},true,false,max_n_group},
 	{LEVEL1,{max_n_group,max_n_group,max_n_group,max_n_group,0},true,false,max_n_group},
-	{LEVEL1,{max_n_group,0,0,0,0},true,false,max_n_group}
+	//{LEVEL1,{0,0,0,0,0},true,false,max_n_group}
 };
 
 
