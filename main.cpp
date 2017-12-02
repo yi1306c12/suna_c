865cb465cb93973d18a1e98f6b2d4f0da6b19c46
yi1306c12 <yi1306c12@gmail.com>
2017-12-03 03:02:12 +0900

    experiment mountaincar

--- main.cpp
+++ main.cpp
@@ -83,12 +83,12 @@ int main()
 
 	Random* random= new State_of_Art_Random(time(NULL));
 	
-	//Reinforcement_Environment* env= new Mountain_Car(random);
+	Reinforcement_Environment* env= new Mountain_Car(random);
 	//Reinforcement_Environment* env= new Function_Approximation(random,1000,false);
 	//Reinforcement_Environment* env= new Single_Cart_Pole(random);
 	//Reinforcement_Environment* env= new Double_Cart_Pole(random);
 	//Reinforcement_Environment* env= new Multiplexer(3,8,random);
-	Reinforcement_Environment* env= new Pendulum_Swing_Up(random);
+	//Reinforcement_Environment* env= new Pendulum_Swing_Up(random);
 	
 
 	//Reinforcement_Agent* agent= new Dummy(env);
