1a14f3b7643ee0cf3e7aa23ecd48c9140e111442
yi1306c12 <yi1306c12@gmail.com>
2017-12-05 01:28:17 +0900

    experiment

--- main.py
+++ main.py
@@ -3,23 +3,23 @@ from environments import double_cart_pole
 from unified_neural_model import unified_neural_model
 import numpy as np
 
-@profile
+#@profile
 def main():
     env = double_cart_pole()
 
     agent = unified_neural_model()
     agent.init(env.observations,env.actions)
 
-    trials = int(3)
+    trials = int(3e5)
     steps = 500
 
     accum_rewards = []
 
     for i in range(trials):
-        print(i)
-#        if i%100 == 0 and i != 0:
-#            print(i//100, max(accum_rewards))
-#            accum_rewards = []
+#        print(i)
+        if i%100 == 0 and i != 0:
+            print(i//100, max(accum_rewards))
+            accum_rewards = []
 
         observation,reward = env.reset(),0
         accum_reward = 0
