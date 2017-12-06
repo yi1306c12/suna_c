7918383561b90ec7db1a24175065136102bc1927
yi1306c12 <yi1306c12@gmail.com>
2017-12-06 14:43:45 +0900

    run with numpy

--- environments_wrapper.cpp
+++ environments_wrapper.cpp
@@ -1,22 +1,23 @@
 #include<boost/python.hpp>
-#include<boost/python/suite/indexing/vector_indexing_suite.hpp>
-#include<vector>
-#include<new>
-using std::vector;
+#include<boost/numpy.hpp>
+namespace p = boost::python;
+namespace np = boost::numpy;
 
-#include"environments/Reinforcement_Environment.h"
+#include<new>
+#include<stdexcept>
+using std::runtime_error;
+
+//#include"environments/Reinforcement_Environment.h"
 #include"environments/Double_Cart_Pole.h"
 #include"random/State_of_Art_Random.h"
-
-#include"converters.hpp"
 
 
 class doublecartpole_python:public Double_Cart_Pole
 {
-public:
-    using double_vector = vector<double>;
+    np::dtype const dtype = np::dtype::get_builtin<double>();
+    p::tuple const stride = p::make_tuple(sizeof(double));
     
-//  typedef vector<double> double_vector;
+public:
     int number_of_observation_vars=-1;
     int number_of_action_vars=-1;
 
@@ -25,50 +26,39 @@ public:
         start(number_of_observation_vars,number_of_action_vars);
     }
 
-    const double step_(vector<double> const& action)
+    const double step_(np::ndarray action, double reward)
     {
-       return Double_Cart_Pole::step(const_cast<double*>(&action.front()));
+        if(action.get_nd() != 1)throw runtime_error("action must be 1-dimensional");
+        return Double_Cart_Pole::step(reinterpret_cast<double*>(action.get_data()));
     }
 
-    vector<double> const& reset()
+    np::ndarray reset()
     {
         restart();
         return last_observation();
     }
 
-    vector<double> const& last_observation()
+    np::ndarray last_observation()
     {
-        lobservation = vector<double>(observation,observation+number_of_observation_vars);
-        return lobservation;
+        p::tuple const shape = p::make_tuple(number_of_observation_vars);//shuold get out
+        return np::from_data(observation, dtype, shape, stride, p::object());
     }
-private:
-    double_vector lobservation;
 };
 
 
 BOOST_PYTHON_MODULE(environments)
 {
     using namespace boost::python;
+    Py_Initialize();
+    np::initialize();
 
     class_<doublecartpole_python>("double_cart_pole")
-        .def("reset",&doublecartpole_python::reset,return_internal_reference<>())
+        .def("reset",&doublecartpole_python::reset)
         .def("step",&doublecartpole_python::step_)
         .def_readonly("observations",&doublecartpole_python::number_of_observation_vars)
         .def_readonly("actions",&doublecartpole_python::number_of_action_vars)
         .def_readonly("MAX_STEPS",&doublecartpole_python::MAX_STEPS)
-        .def("last_observation",&doublecartpole_python::last_observation,return_internal_reference<>())
+        .def("last_observation",&doublecartpole_python::last_observation)
         .def_readonly("trial",&doublecartpole_python::trial)
-    ;
-
-    to_python_converter<vector<double>, vector_to_pylist_converter<vector<double> > >();
-
-    converter::registry::push_back(
-        &pylist_to_vector_converter<vector<double> >::convertible,
-        &pylist_to_vector_converter<vector<double> >::construct,
-        boost::python::type_id<vector<double> >()
-    );
-
-    class_<doublecartpole_python::double_vector>("double_vector")
-        .def(vector_indexing_suite<doublecartpole_python::double_vector>())
     ;
 }
