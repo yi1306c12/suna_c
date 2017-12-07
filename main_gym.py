722de4126f70b60c80d991139905ca4c46868768
yi1306c12 <yi1306c12@gmail.com>
2017-12-07 18:11:15 +0900

    lunarlander

--- main_gym.py
+++ main_gym.py
@@ -1,17 +1,19 @@
 import numpy as np
+import gym
+from unified_neural_model import unified_neural_model
 
 @profile
 def main():
-    import gym
     env = gym.make('BipedalWalker-v2')
+#    env = gym.make('LunarLanderContinuous-v2')
+
     inputs,outputs = env.observation_space.shape[0], env.action_space.shape[0]#pendulum,lunarlander,bipedwalker,MountainCarContinuous
 
-    from unified_neural_model import unified_neural_model
     agent = unified_neural_model()
     agent.init(inputs,outputs)
 
-    trials = int(3000)
-    steps = 500
+    trials = int(3e5)
+    steps = 2000
 
     accum_rewards = []
     for i in range(trials):
@@ -23,11 +25,14 @@ def main():
         accum_reward = 0
         for t in range(steps):
             action = agent.step(observation,reward)
+#            action = np.clip(action,-1.,1.)#lunarlander
+
             observation, reward, done, info = env.step(action)
             #env.render()
             accum_reward += reward
             if done:
                 break
+#                observation = env.reset()
         agent.endEpisode(reward)
         accum_rewards.append(accum_reward)
 
