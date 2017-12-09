8e0aa74cee4b174db43eb0c8d287b18fa2eccdaa
yi1306c12 <yi1306c12@gmail.com>
2017-12-09 17:54:53 +0900

    retry for more leaning

--- main_gym.py
+++ main_gym.py
@@ -13,7 +13,8 @@ def main():
     agent.init(inputs,outputs)
 
     trials = int(3e5)
-    steps = 2000
+    steps = 1000
+    retry = 10
 
     accum_rewards = []
     for i in range(trials):
@@ -21,18 +22,19 @@ def main():
             print(i, max(accum_rewards))
             accum_rewards = []
 
-        observation,reward = env.reset(),0
         accum_reward = 0
-        for t in range(steps):
-            action = agent.step(observation,reward)
-#            action = np.clip(action,-1.,1.)#lunarlander
+        for r in range(retry):
+            observation,reward = env.reset(),0
+            for t in range(steps):
+                action = agent.step(observation,reward)
+    #            action = np.clip(action,-1.,1.)#lunarlander
 
-            observation, reward, done, info = env.step(action)
-            #env.render()
-            accum_reward += reward
-            if done:
-                break
-#                observation = env.reset()
+                observation, reward, done, info = env.step(action)
+                #env.render()
+                accum_reward += reward
+                if done:
+                    break
+    #                observation = env.reset()
         agent.endEpisode(reward)
         accum_rewards.append(accum_reward)
 
