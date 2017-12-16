26aaccc519a8c0134b2dd60f67ba679906316551
yi1306c12 <yi1306c12@gmail.com>
2017-12-16 19:16:17 +0900

    close test

--- dummy.py
+++ dummy.py
@@ -2,19 +2,20 @@ import gym
 
 @profile
 def memory_test():
-    env = gym.make('BipedalWalker-v2')
-    trials = int(3000)
+    trials = int(300)
     retry = 10
     steps = 1000
 
     accum_rewards = []
     for i in range(trials):
         print(i)
+        env = gym.make('BipedalWalker-v2')
         for r in range(retry):
             observation,reward = env.reset(),0
             for t in range(steps):
                 observation, reward, done, info = env.step(env.action_space.sample())
                 if done:
                     break
+        env.close()
 
 memory_test()
