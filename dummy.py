14458a46f7ff4431685dcca1ad4a1b8ee712c1a6
yi1306c12 <yi1306c12@gmail.com>
2017-12-16 20:42:49 +0900

    show current process memory

--- dummy.py
+++ dummy.py
@@ -1,21 +1,26 @@
 import gym
+import psutil
+import os
+pid = os.getpid()
+py = psutil.Process(pid)
 
 @profile
 def memory_test():
-    trials = int(300)
+    trials = int(1000)
     retry = 10
     steps = 1000
 
     accum_rewards = []
+    env = gym.make('BipedalWalker-v2')
     for i in range(trials):
-        print(i)
-        env = gym.make('BipedalWalker-v2')
+        print(i, "memory usage:{}MB".format(py.memory_info()[0]/2**20))
         for r in range(retry):
             observation,reward = env.reset(),0
             for t in range(steps):
                 observation, reward, done, info = env.step(env.action_space.sample())
                 if done:
                     break
-        env.close()
+                    
+        
 
 memory_test()
