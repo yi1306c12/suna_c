a6b70c968e02b0fae50756b37e36a5873acacf79
yi1306c12 <yi1306c12@gmail.com>
2017-12-16 23:39:22 +0900

    devide in function

--- main_gym.py
+++ main_gym.py
@@ -1,44 +1,47 @@
+import os
+pid = os.getpid()
+import psutil
+py = psutil.Process(pid)
+
 import numpy as np
 import gym
 from unified_neural_model import unified_neural_model
 
-@profile
-def main():
-    env = gym.make('BipedalWalker-v2')
-#    env = gym.make('LunarLanderContinuous-v2')
+env = gym.make('BipedalWalker-v2')
+#env = gym.make('LunarLanderContinuous-v2')
 
-    inputs,outputs = env.observation_space.shape[0], env.action_space.shape[0]#pendulum,lunarlander,bipedwalker,MountainCarContinuous
+inputs,outputs = env.observation_space.shape[0], env.action_space.shape[0]#pendulum,lunarlander,bipedwalker,MountainCarContinuous
 
-    agent = unified_neural_model()
-    agent.init(inputs,outputs)
+agent = unified_neural_model()
+agent.init(inputs,outputs)
 
-    trials = int(3e5)
-    steps = 1000
-    retry = 10
+trials = int(3e5)
+steps = 1000
+retry = 10
 
-    accum_rewards = []
-    for i in range(trials):
-        if i%100 == 0 and i != 0:
-            print(i, max(accum_rewards))
-            accum_rewards = []
+def trial():
+    accum_reward = 0
+    for r in range(retry):
+        observation,reward = env.reset(),0
+        for t in range(steps):
+            action = agent.step(observation,reward)
+#            action = np.clip(action,-1.,1.)#lunarlander
 
-        accum_reward = 0
-        for r in range(retry):
-            observation,reward = env.reset(),0
-            for t in range(steps):
-                action = agent.step(observation,reward)
-    #            action = np.clip(action,-1.,1.)#lunarlander
+            observation, reward, done, info = env.step(action)
+            #env.render()
+            accum_reward += reward
+            if done:
+                break
+#                observation = env.reset()
+    agent.endEpisode(reward)
+    return accum_reward
 
-                observation, reward, done, info = env.step(action)
-                #env.render()
-                accum_reward += reward
-                if done:
-                    break
-    #                observation = env.reset()
-        agent.endEpisode(reward)
-        accum_rewards.append(accum_reward)
+accum_rewards = []
+for i in range(trials):
+    if i%100 == 0 and i != 0:
+        print(i, max(accum_rewards), "memory_usage:{}MB".format(py.memory_info()[0]/2**20))
+        accum_rewards = []
 
-    agent.saveAgent("dna_best_individual")
+    accum_rewards.append(trial())
 
-if __name__=='__main__':
-    main()
+agent.saveAgent("dna_best_individual")
