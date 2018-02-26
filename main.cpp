63a53c76f1ff85e92c1e6b27676c63a42d2c3b7a
yi1306c12 <yi1306c12@gmail.com>
2018-02-26 15:07:49 +0900

    rewrite main.cpp

--- main.cpp
+++ main.cpp
@@ -16,111 +16,34 @@
 
 #include"parameters.h"
 
-FILE* main_log_file;
+#include<limits>
 
-double computeAverage(double* last_rewards, int counter)
-{
-	int k;
-
-	if(counter>100)
-	{
-		counter=100;
-	}
-
-	double avg_rewards=0.0;
-	//printf("AAA\n");
-	for(k=0;k<counter;++k)
-	{
-		//printf("%f\n",last_rewards[k]);
-		avg_rewards+= last_rewards[k];
-	}
-	avg_rewards= avg_rewards/(double)counter;
-
-	return avg_rewards;
-}
-
-void setFeatures(Reinforcement_Environment* env)
-{
-	
-#ifdef SET_NORMALIZED_INPUT
-	bool feature_available;
-	
-	feature_available= env->set(NORMALIZED_OBSERVATION);
-
-	if(feature_available == false)
-	{
-		printf("NORMALIZED_OBSERVATION feature not available\n");
-		exit(1);
-	}
-	else
-	{
-		fprintf(main_log_file,"Normalized Observation enabled\n");
-	}
-#endif
-
-#ifdef SET_NORMALIZED_OUTPUT
-	bool feature_available;
-	
-	feature_available= env->set(NORMALIZED_ACTION);
-
-	if(feature_available == false)
-	{
-		printf("NORMALIZED_ACTION feature not available\n");
-		exit(1);
-	}
-	else
-	{
-		fprintf(main_log_file,"Normalized Action enabled\n");
-	}
-#endif
-
-}
+int const trial_frequency_to_print= 100;
 
 int main()
 {
-	//int trials_to_change_maze_states= 10000;
 	int i;
-	main_log_file= fopen("log.txt","w");
 
 	Random* random= new State_of_Art_Random(time(NULL));
 	
 	//Reinforcement_Environment* env= new Mountain_Car(random);
 	//Reinforcement_Environment* env= new Function_Approximation(random,1000,false);
 	//Reinforcement_Environment* env= new Single_Cart_Pole(random);
-	//Reinforcement_Environment* env= new Double_Cart_Pole(random);
+	Reinforcement_Environment* env= new Double_Cart_Pole(random);
 	//Reinforcement_Environment* env= new Multiplexer(3,8,random);
 	//Reinforcement_Environment* env= new Pendulum_Swing_Up(random);
-	Reinforcement_Environment* env = new Count_Minority(random);
+	//Reinforcement_Environment* env = new Count_Minority(random);
 	
 
-	//Reinforcement_Agent* agent= new Dummy(env);
 	Reinforcement_Agent* agent= new Unified_Neural_Model(random);
-	
-	setFeatures(env);
-
-	//Self_Organizing_Neurons* b= (Self_Organizing_Neurons*)agent;
-
 	//print max accumulated reward seen in N trials, the N trials is given by trial_frequency_to_print
-	bool print_max_accum_reward_in_n_trials= true;
-	int trial_frequency_to_print= 100;
 	double max_accum_reward=0;
 	bool was_initialized=false;	//tells if the max_accum_reward was initialized
 
-	bool print_reward=false;
-	bool print_step=false;
-	bool print_average=false;
-	//bool print_accumulated_reward=true;
-	bool print_agent_information=false;
-
-	//int trials=100000
 	int trials=300000;		
-	//int trials=200;		
-	//int trials=500;		
-	//int trials=100000;		
 	
 	int number_of_observation_vars;
 	int number_of_action_vars;
-	
 	env->start(number_of_observation_vars, number_of_action_vars);
 	agent->init(number_of_observation_vars, number_of_action_vars);
 	
@@ -141,67 +64,31 @@ int main()
 		//do one trial (multiple steps until the environment finish the trial or the trial reaches its MAX_STEPS)
 		while(env->trial==i && step_counter <= env->MAX_STEPS)
 		{
-
 			agent->step(env->observation, reward);
-
-			reward= env->step(agent->action);		
-		
+			reward= env->step(agent->action);
 			accum_reward+= reward;
-		
-			if(print_reward)
-			{
-				last_rewards[counter%100]=reward;
-				counter++;
-				
-				if(print_average)
-				{
-					avg_rewards= computeAverage(last_rewards, counter);
-					printf("%d %f\n",i, avg_rewards);
-				}
-				else
-				{
-					printf("%d %f\n",i, reward);
-				}
-
-
-			}
-		
 			step_counter++;
-
 		}
-
-#ifdef TERMINATE_IF_MAX_STEPS_REACHED
-		//end evolution when the MAX_STEPS is reached
-		if(step_counter > env->MAX_STEPS)
-		{
-			i=trials;
-		}
-#endif
 	
 		//update the max_accum_reward and print		
-		if(print_max_accum_reward_in_n_trials)
-		{	
-			if(was_initialized==false)
-			{
-				was_initialized=true;
-				max_accum_reward= accum_reward;
-			}
-			else
-			{
-				if(max_accum_reward < accum_reward)
-				{
-					max_accum_reward= accum_reward;
-				}
-			}		
-		
-			if(i%trial_frequency_to_print==0)
-			{	
-				printf("%d %f\n",i, max_accum_reward);
-		
-				max_accum_reward=0;
-				was_initialized=false;
+		if(max_accum_reward < accum_reward)
+		{
+			max_accum_reward= accum_reward;
+		}
 
-			}
+#ifdef STOP_REWARD
+		if(accum_reward > STOP_REWARD)
+		{
+			printf("%d %f\n",i,max_accum_reward);
+			break;
+		}
+#endif
+
+
+		if(i%trial_frequency_to_print==0)
+		{	
+			printf("%d %f\n",i, max_accum_reward);
+			max_accum_reward=std::numeric_limits<float>::min();
 		}
 	
 		agent->endEpisode(reward);
@@ -217,52 +104,14 @@ int main()
 		{
 			reward= env->step(NULL);
 		}
-	
-		//print the number of steps used in the last trial
-		if(print_step)
-		{
-			last_rewards[counter%100]=step_counter;
-			counter++;
-
-			if(print_average)
-			{
-				avg_rewards= computeAverage(last_rewards, counter);
-				printf("%d %f\n",i, avg_rewards);
-			}
-			else
-			{
-				printf("%d %f\n",i, step_counter);
-			}
-		}
 		
-		if(print_agent_information==true)
-		{
-			agent->print();
-		}
-
 		step_counter=1;
-
 		i++;
 
-#ifdef STOP_REWARD
-		if(max_accum_reward > STOP_REWARD)
-		{
-			printf("%d %f\n",i,max_accum_reward);
-			break;
-		}
-#endif
 	
 	}
 
 	agent->saveAgent("dna_best_individual");
-	
-
-
-	//printf("reward average %f\n",reward_sum/(double)trials);
-	//printf("step average %f\n",step_sum/(double)trials);
-	
-	fclose(main_log_file);	
-
 	return 0;
 }
 
