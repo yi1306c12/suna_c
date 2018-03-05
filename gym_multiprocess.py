4a133aa4fbaef6ac1b54707c88ba18fe875c014c
yi1306c12 <yi1306c12@gmail.com>
2018-03-06 00:47:15 +0900

    limited observation

--- gym_multiprocess.py
+++ gym_multiprocess.py
@@ -1,7 +1,38 @@
 from multiprocessing import Process,Queue
 import gym
 environment_name = 'BipedalWalker-v2'
+input_select = set([
+#   0,	#hull_angle
+#   1,	#hull_angularVelocity
+#   2,	#vel_x
+#   3,	#vel_y
+    4,	#hip_joint_1_angle
+#   5,	#hip_joint_1_speed
+    6,	#knee_joint_1_angle
+#   7,	#knee_joint_1_speed
+    8,	#leg_1_ground_contact_flag
+    9,	#hip_joint_2_angle
+#   10,	#hip_joint_2_speed
+    11,	#knee_joint_2_angle
+#   12,	#knee_joint_2_speed
+    13	#leg_2_ground_contact_flag
+#   14-23   #10 lidar readings
+])
 gym.logger.set_level(40)#exclude waning message
+
+class limited_observation:
+    def __init__(self,env,limitation):
+        self.env = env
+        self.limitation = limitation
+
+    def step(self,action):
+        observation, reward, done, info = self.env.step(action)
+        return observation[input_select], reward, done, info
+
+    def reset(self):
+        return self.env.reset()[input_select]
+
+
 from unified_neural_model import unified_neural_model
 
 import os
@@ -11,7 +42,8 @@ py = psutil.Process(pid)
 
 
 def f(agent,queue,steps,retry):
-    env = gym.make(environment_name)
+#    env = gym.make(environment_name)
+    env = limited_observation(gym.make(environment_name), input_select)
     observation, reward = env.reset(), 0
     
     sum_reward = 0
@@ -29,7 +61,8 @@ def f(agent,queue,steps,retry):
 
 def main(generations, steps, retry):
     env = gym.make(environment_name)
-    inputs, outputs = env.observation_space.shape[0], env.action_space.shape[0]
+#    inputs, outputs = env.observation_space.shape[0], env.action_space.shape[0]
+    inputs, outputs = len(input_select), env.action_space.shape[0]
 
     agent = unified_neural_model()
     agent.init(inputs,outputs)
