de4486b916f227e739d8d772e95803dcdcec282d
yi1306c12 <yi1306c12@gmail.com>
2018-11-13 17:01:32 +0900

    mutation & weight test

--- module_wrapper_test.py
+++ module_wrapper_test.py
@@ -1,3 +1,13 @@
 from module_wrapper import module
 
 m = module(2, 2)
+m.structuralMutation(200)
+for i in range(100):
+    m.weightMutation()
+
+
+import numpy
+observation = numpy.array([1,1])
+for i in range(10):
+    print(m.process(observation))
+
