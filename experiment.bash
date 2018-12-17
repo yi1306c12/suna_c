e97753f3083430cc5c0e011db208972c1223c19c
yi1306c12 <yi1306c12@gmail.com>
2018-12-17 12:31:47 +0900

    optional argument

--- experiment.bash
+++ experiment.bash
@@ -1,1 +1,1 @@
-parallel --noswap --joblog joblog.txt --bar -j $2 './rl "environment_setting{}.txt" "best_individual{}.dot" "dna_best_individaul{}" > "score{}.txt"' ::: `seq 1 $1`
+parallel --noswap --joblog joblog.txt --bar -j $2 './rl "environment_setting{1}.txt" "best_individual{1}.dot" "dna_best_individaul{1}" --neat_parameters {2} > "score{1}.txt"' ::: `seq 1 $1` ::: $3
